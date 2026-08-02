c20fc7867672fc2d76d17433c57b65a0540d20e5
yiheng <105923054+pekyiheng@users.noreply.github.com>
2026-08-02 23:22:56 +0800

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int highest = 0;
+        unordered_map<int, int> m;
+
+        for (int num : nums) {
+            int cur = 1;
+            if (m[num - 1]) {
+                cur += m[num -1];
+            }
+
+            m[num] = cur;
+
+            while (m[++num] > 0) {
+                ++cur;
+                m[num] = cur;
+            }
+
+            if (cur > highest) {
+                highest = cur;
+            }
+        }
+
+        return highest;
+        
+    }
+};
